b99886a1ffb8579da34e50dd112360433abd9ea1
bankoja <james@imac03.local>
2017-02-28 13:53:25 -0500

    Initial Commit

---
+++ SSFS Upper School/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SSFS Upper School
+//
+//  Created by james on 2/28/17.
+//  Copyright © 2017 james. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
