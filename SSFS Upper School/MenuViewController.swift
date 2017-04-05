33c411e6105b8d6b94f17490d5bdb44377d33761
bankoja <james@imac03.local>
2017-04-05 14:34:56 -0400

    Progress Check 2

---
+++ SSFS Upper School/MenuViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  SSFS Lunch
+//
+//  Created by Claire Youmans on 4/25/16.
+//  Copyright © 2016 Claire Youmans. All rights reserved.
+//
+
+//Should I make it so that if the lunch isn't up on the website, it says "lunch unavailable currently" or something?
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    var menu = Menu()
+    
+    var weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
+    @IBOutlet weak var lunchEntreeText: UITextView!
+    
+    
+    
+    @IBOutlet weak var vegetarianEntreeLabel: UILabel!
+    @IBOutlet weak var vegetarianEntreeText: UITextView!
+    
+    
+    
+    
+    @IBOutlet weak var sidesLabel: UILabel!
+    @IBOutlet weak var sidesText: UITextView!
+    
+    
+    @IBOutlet weak var downtownDeliLabel: UILabel!
+    @IBOutlet weak var downtownDeliText: UITextView!
+    
+    
+    
+    
+    func dayOfWeek() {
+        var dayString = String()
+        var dayBool = Bool()
+        let dayOfWeek = getCurrentDay()
+        if dayOfWeek! == 2 {
+            dayString = "MONDAY(.*?)TUESDAY"
+            dayBool = false
+        } else if dayOfWeek == 3 {
+            dayString = "TUESDAY(.*?)WEDNESDAY"
+            dayBool = false
+        } else if dayOfWeek == 4 {
+            dayString = "WEDNESDAY(.*?)THURSDAY"
+            dayBool = false
+        } else if dayOfWeek == 5 {
+            dayString = "THURSDAY(.*?)FRIDAY"
+            dayBool = false
+        } else if dayOfWeek == 6 {
+            dayString = "FRIDAY(.*)"
+            dayBool = true
+        }
+        // This function links the return from the getCurrentDay function to the corresponding weekday and gets the information for the day by using the words in between that day and the following
+        let day = DailyMenu(regExText: dayString, isFriday: dayBool)
+        dateLabel.text = weekdays[dayOfWeek! - 2]
+        lunchEntreeText.text = day.lunchEntree
+        vegetarianEntreeText.text = day.vegetarianEntree
+        sidesText.text = day.sides
+        downtownDeliText.text = day.downtownDeli
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dayOfWeek()
+       
+    }
+    
+    func getCurrentDay()->Int?{
+        let date = Date()
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .weekday], from: date)
+        let day = components.weekday
+        
+        return day
+        // code from http://stackoverflow.com/questions/28861091/getting-the-current-day-of-the-week-in-swift .This function gets the current day of the week and returns it as Optional() with the corresponding number for the day of the week it is. For example Optional(2) is Monday and Optional(1) is Sunday.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
