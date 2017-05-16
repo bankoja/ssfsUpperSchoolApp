438d20075120a6bbb41fbb660dc203384187d66c
bankoja <james@iMac03.local>
2017-05-16 13:34:56 -0400

    link library calendar

--- SSFS Upper School/AfterSchool.swift
+++ SSFS Upper School/AfterSchool.swift
@@ -29,11 +29,13 @@ class AfterS {
         let name = processRegEx(regExText: regExText, searchText: activity)
         let regExTextTwo = "href=\"#\">(.*?)</"
         let game = processRegEx(regExText: regExTextTwo, searchText: name)
-        print(game)
         return game
+        // This function contains the search string.
     }
+}
     func processRegEx(regExText: String, searchText:String) -> String {
         var name: String = ""
+        var returnGames: String = ""
         if let range = searchText.range(of:regExText) {
             name = searchText.substring(with:range)
         }
@@ -42,18 +44,16 @@ class AfterS {
             let matches = regex.matches(in: searchText as String, options: [], range: NSMakeRange(0, searchText.characters.count))
             for match in matches{
                 name = (searchText as NSString).substring(with: match.rangeAt(1))
+                returnGames += "\n" + name + "\n"
                 
             }
-            if let match = matches.first {
-                name = (searchText as NSString).substring(with: match.rangeAt(1))
                 
-                // These lines of code isolate the data that I intend to pull from the url. The function, getGames, uses a regular expression, regExText, and takes a small portion of info from the page source by identifying where the date of each event is. It calls the dayOfWeek variable to find all portions of information that correspond with the current date. It then sets that equal to name. The next regular expression, regExTwo, takes that portion of information from name and then pulls the title of the event from it. I am now trying to get the processRegEx function to do it for each event, I am currently unsuccessful.  
-            }
+                // These lines of code isolate the data that I intend to pull from the url. The function, getGames, uses a regular expression, regExText, and takes a small portion of info from the page source by identifying where the date of each event is. It calls the dayOfWeek variable to find all portions of information that correspond with the current date. It then sets that equal to name. The next regular expression, regExTwo, takes that portion of information from name and then pulls the title of the event from it. I am now trying to get the processRegEx function to do it for each event, I am currently unsuccessful.
             
         } catch {
         }
-        return name
+        return returnGames
     }
     
-}
+
 
--- SSFS Upper School/AfterSchoolViewController.swift
+++ SSFS Upper School/AfterSchoolViewController.swift
@@ -10,17 +10,27 @@ import UIKit
 
 class AfterSchoolViewController: UIViewController {
     let afterSchool = AfterS()
+    var weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var gamesTodayText: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         let activities = afterSchool.getGames(dayOfWeek: getCurrentDay()!)
-        print(activities)
+        gamesTodayText.text = activities
+        dayOfWeek()
         // Do any additional setup after loading the view. The "let activities" sends the date to the getGames function in my after school file, and then runs it in this file.
+    }
+    func dayOfWeek() {
+        let dayOfWeek = getCurrentDay()
+        //dateLabel.text = weekdays[dayOfWeek! - 1]
+        // pasted into here to get day of the week at the top of the screen (See MenuViewController)
     }
     func getCurrentDay()->Int?{
         let date = Date()
         let calendar = Calendar.current
         let components = calendar.dateComponents([.year, .month, .day], from: date)
         let day = components.day
+        // pasted into here to get day of the week at the top of the screen (See MenuViewController)
         
         return day
         // code from http://stackoverflow.com/questions/28861091/getting-the-current-day-of-the-week-in-swift . This code gets the day of the current date as an integer (Unlike code in menu view controller, this returns the date instead of the day of week).
